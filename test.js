668230f1ae8dffa281ec85e83c534d17d674e9c2
Nakib <nbmomin14@gmail.com>
2016-12-07 21:38:51 -0800

    Added Sugurus’s Test File

---
+++ test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
+
+
+const argv = require('minimist')(process.argv.slice(2));
+const target = argv.target || argv.t;
+const reg = new RegExp(`${target||'.*'}`);
+
+const selfpath = path.resolve(__dirname, 'test.js');
+
+(function resolve(dirpath, _exports) {
+  _.forEach(fs.readdirSync(dirpath), filename => {
+    const filepath = path.resolve(dirpath, filename);
+    if (/^(node_modules|.git)$/.test(filename) || selfpath === filepath) {
+      return;
+    }
+    if (fs.statSync(filepath).isDirectory()) {
+      return resolve(filepath, _exports[filename] = {});
+    }
+    if (/^test(.*).js$/.test(filename) && reg.test(filepath)) {
+      require(filepath);
+    }
+  });
+})(__dirname, exports);
